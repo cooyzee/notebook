6270f0ba303f2a166a85eb18e5f0504a7dff22ef
cooyzee@gmail.com <cooyzee@gmail.com>
2020-02-06 10:16:16 +0800

    notebook initialization

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# notebook
-Cooyzee's precious notebook.
+# Cooyzee
+Cooyzee's front working set.
+
+> Cause there ain't nobody, ain't nobody loves you like I love you.
+
+### End
+* Learning.
+* Be strong!
+* Keep forward.
+* And be happy, the most important!
---
+++ test.js
@@ -0,0 +1,17 @@
+/** delete duplicate items of a array
+ **/
+const fruits = ['banana', 'apple', 'pear', 'watermelon', 'orange', 'apple']
+
+// const uniqueFruits = Array.from(new Set(fruits))
+const uniqueFruits = [...new Set(fruits)]
+
+/** delete duplicate items of a array
+ **/
+
+const x = new Array(5).fill(1)
+x.length = 0
+x.length = 3
+
+const temp = undefined
+console.log(typeof temp)
+
